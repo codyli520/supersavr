dfdf3bbc2e98192518d83b305a9c63716fe7dcc0
Longfei Li <codyli520@gmail.com>
2016-02-12 11:37:55 +0000

    fixed new user google chart display issue

--- routes/signup.js
+++ routes/signup.js
@@ -1,7 +1,7 @@
 var data = require("../data.json");
 exports.view = function(req, res){  
 	// Your code goes here
-    var newUser = {"username": req.query.username, "password": req.query.password, "goal": req.query.goal}
+    var newUser = {"username": req.query.username, "password": req.query.password, "goal": req.query.goal, "saving" : "0"}
     console.log("name is: " + newUser.username + " " + "password is: " + newUser.password + " goal is: " + newUser.goal);
     
 	data["user"].push(newUser);
