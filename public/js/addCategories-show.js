1347a98ba6e820d40af8e996d245fbe91fc71c4c
Longfei Li <codyli520@gmail.com>
2016-03-01 19:33:50 -0800

    delete a comment

--- public/js/addCategories-show.js
+++ public/js/addCategories-show.js
@@ -5,7 +5,6 @@ $( document ).ready(function() {
     $('#groceryadd-btn').click(function(e){
         var curTime = new Date();
         var time = curTime - pageLoadTime;
-        alert(time);
         ga('send','timing','category', 'selected',time);
         ga('send','event','category','click');
     });
