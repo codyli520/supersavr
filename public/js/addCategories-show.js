b46cbbd581dc269803e75c70562967da9e3ef491
Longfei Li <codyli520@gmail.com>
2016-03-01 19:13:38 -0800

    added google analytics

---
+++ public/js/addCategories-show.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {  
+   initializePage();
+    var pageLoadTime = new Date();
+    
+    $('#groceryadd-btn').click(function(e){
+        var curTime = new Date();
+        var time = curTime - pageLoadTime;
+        alert(time);
+        ga('send','timing','category', 'selected',time);
+        ga('send','event','category','click');
+    });
+});
+
+function initializePage() {
+ 
+}
