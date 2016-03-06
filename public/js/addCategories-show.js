a7ff12378c27cbe05b4fd64f8982789b1ef6e12c
Longfei Li <codyli520@gmail.com>
2016-03-06 08:18:15 -0800

    make v1 category page the same as v2

--- public/js/addCategories-show.js
+++ public/js/addCategories-show.js
@@ -2,18 +2,13 @@ $( document ).ready(function() {
    initializePage();
     var pageLoadTime = new Date();
     
-    $('#groceryadd-btn').click(function(e){
+    $('.thumbnail').click(function(e){
         var curTime = new Date();
         var time = curTime - pageLoadTime;
         ga('send','timing','category', 'selected',time);
     });
-
-   
 });
 
 function initializePage() {
  
 }
-
-
-
