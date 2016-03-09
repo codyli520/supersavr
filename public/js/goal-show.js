e1b6d7e7a207cac5f2f65365c6b64dae120d9415
Longfei Li <codyli520@gmail.com>
2016-03-08 21:57:25 -0800

    enforce goal entry to be numerical

--- public/js/goal-show.js
+++ public/js/goal-show.js
@@ -38,4 +38,16 @@ function addItem(result){
 function deleteItem(id){
     $("#"+id).addClass("hidden");
 }
+
+function validate(evt) {
+  var theEvent = evt || window.event;
+  var key = theEvent.keyCode || theEvent.which;
+  key = String.fromCharCode( key );
+  var regex = /[0-9]|\./;
+  if( !regex.test(key) ) {
+    theEvent.returnValue = false;
+    if(theEvent.preventDefault) theEvent.preventDefault();
+  }
+}
+
     
