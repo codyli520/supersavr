5ea5b005ea099fb36f31d5e34330da1f1e14eb99
Longfei Li <codyli520@gmail.com>
2016-03-08 21:45:13 -0800

    new goal, edit goal, image relink ..etc.

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,25 @@ exports.view = function(req, res){
     console.log(data);
     for (var i = 0; i < data["user"].length; i++) {
         if(username == data["user"][i].username){
+            
+            if(req.query.newGoal != null){
+                data["user"][i].goal = req.query.newGoal;
+                data["user"][i].saving = 0;
+            }
+            
             var goal = data["user"][i].goal;
             var saving = data["user"][i].saving;
             var amountNeeded = (parseInt(data["user"][i].goal)- parseInt(data["user"][i].saving)).toString()
-             data["user"][i].needs = amountNeeded;
+            if(parseInt(amountNeeded) < 0)
+                data["user"][i].needs = 0
+            else
+                data["user"][i].needs = amountNeeded;
             var needs = data["user"][i].needs;
-            res.render('index', {"username": username, "goal": goal, "saving": saving, "needs": needs })
+            
+            if(parseInt(needs) == 0)
+                res.render('indexNewGoal', {"username": username, "goal": goal, "saving": saving, "needs": needs })
+            else
+                res.render('index', {"username": username, "goal": goal, "saving": saving, "needs": needs})
         }
     }
 };
